fbc73d3597ffc27b75af07886823d99fd9ff2a70
gabriela_m_amorim <gabimaciellopesamorim@gmail.com>
2025-05-09 16:59:22 -0300

    Estrutura pronta

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+          primary: '#ff5733',
+          secondary: '#33ff57',
+        },
+        fontSize: {
+          'xs': '.75rem',
+          'sm': '.875rem',
+          'tiny': '.875rem',
+          'base': '1rem',
+          'lg': '1.125rem',
+          'xl': '1.25rem',
+          '2xl': '1.5rem',
+          '3xl': '1.875rem',
+          '4xl': '2.25rem',
+          '5xl': '3rem',
+          '6xl': '4rem',
+          '7xl': '5rem',
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+
+
+
+  }
